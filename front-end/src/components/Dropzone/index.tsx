df4a36937373f53f559b884eef9a125b6b93cb01
Henrique Tavares <ihenrits@gmail.com>
2020-06-07 14:42:40 -0300

    adicionando imagem ao cadastrar ponto de coleta no front-end

---
+++ front-end/src/components/Dropzone/index.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+import './styles.css';
+
+interface Props {
+  onFileUpload: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUpload }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback(
+    acceptedFiles => {
+      const file = acceptedFiles[0];
+
+      const filesUrl = URL.createObjectURL(file);
+
+      setSelectedFileUrl(filesUrl);
+      onFileUpload(file);
+    },
+    [onFileUpload],
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+  });
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="Point Thumbnail" />
+      ) : (
+        <p>
+          <FiUpload />
+          Imagem do estabelecimento
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;
--- front-end/src/pages/CreatePoint/index.tsx
+++ front-end/src/pages/CreatePoint/index.tsx
@@ -13,6 +13,8 @@ import axios from 'axios';
 
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import logo from '../../assets/logo.svg';
 
 import './styles.css';
@@ -55,6 +57,7 @@ const CreatePoint: React.FC = () => {
     0,
   ]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -155,16 +158,20 @@ const CreatePoint: React.FC = () => {
       const [latitude, longitude] = selectedPosition;
       const itemsData = selectedItems;
 
-      const data = {
-        name,
-        email,
-        whatsapp,
-        uf,
-        city,
-        latitude,
-        longitude,
-        items: itemsData,
-      };
+      const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', itemsData.join(','));
+
+      if (selectedFile) {
+        data.append('image', selectedFile);
+      }
 
       await api.post('points', data);
 
@@ -176,6 +183,7 @@ const CreatePoint: React.FC = () => {
       formData,
       history,
       selectedCity,
+      selectedFile,
       selectedItems,
       selectedPosition,
       selectedUf,
@@ -197,6 +205,8 @@ const CreatePoint: React.FC = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUpload={setSelectedFile} />
 
         <fieldset>
           <legend>
